306e89c949c3705e5eeb4376b58ff201d0aa6ab6
StepByStep <162555559+StefMila@users.noreply.github.com>
2026-05-09 15:37:11 +0200

    testato il backend con le modifiche di elena. tutto ancora funzionante. Ho commentato il file index che da errore perchè non abbiamo il file swagger.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Andando su localhost:3000/api-docs avrai una documentazione delle tue API.
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json' with { type: 'json' };
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerDocument from './swagger.json' with { type: 'json' };
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
